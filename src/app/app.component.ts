2183198103fe742f86f9e971bb420a0d1c08079f
aaj013 <aaj013@live.com>
2017-09-07 12:04:40 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Bot UI';
+  message = '';
+
+  onClickMe(value) {
+    this.message = this.message+'Input : '+value;
+  }
 }
